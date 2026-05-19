5377b83dd893cb1b329ce6553f31a4892c46c97a
hafizahputrinoer12 <hafizahputrinoer@gmail.com>
2026-05-19 11:30:18 +0700

    Update tambah.php

--- Pekan 13/Putri Noer Hafizah_008/tambah.php
+++ Pekan 13/Putri Noer Hafizah_008/tambah.php
@@ -1,1 +1,33 @@
+<?php
+include 'koneksi.php';
 
+if(isset($_POST['simpan'])){
+    $nim  = $_POST['nim'];
+    $nama = $_POST['nama'];
+
+    mysqli_query($conn, "INSERT INTO mahasiswa VALUES('', '$nim', '$nama')");
+    header("location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Data</title>
+</head>
+<body>
+
+<h2>Tambah Data</h2>
+
+<form method="post">
+    NIM <br>
+    <input type="text" name="nim"><br>
+
+    Nama <br>
+    <input type="text" name="nama"><br><br>
+
+    <input type="submit" name="simpan" value="Simpan">
+</form>
+
+</body>
+</html>
