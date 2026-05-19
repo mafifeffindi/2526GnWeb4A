ede839688c15ab58687e2e4017746c8c05e2f07a
hafizahputrinoer12 <hafizahputrinoer@gmail.com>
2026-05-19 11:30:44 +0700

    Update edit.php

--- Pekan 13/Putri Noer Hafizah_008/edit.php
+++ Pekan 13/Putri Noer Hafizah_008/edit.php
@@ -1,1 +1,40 @@
+<?php
+include 'koneksi.php';
 
+$id = $_GET['id'];
+$data = mysqli_query($conn, "SELECT * FROM mahasiswa WHERE id='$id'");
+$d = mysqli_fetch_array($data);
+
+if(isset($_POST['update'])){
+    $nim  = $_POST['nim'];
+    $nama = $_POST['nama'];
+
+    mysqli_query($conn, "UPDATE mahasiswa 
+        SET nim='$nim', nama='$nama' 
+        WHERE id='$id'");
+
+    header("location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Data</title>
+</head>
+<body>
+
+<h2>Edit Data</h2>
+
+<form method="post">
+    NIM <br>
+    <input type="text" name="nim" value="<?php echo $d['nim']; ?>"><br>
+
+    Nama <br>
+    <input type="text" name="nama" value="<?php echo $d['nama']; ?>"><br><br>
+
+    <input type="submit" name="update" value="Update">
+</form>
+
+</body>
+</html>
