3872998e75196e29542fa5e063ad7a7985dac431
arum035 <arumarum7676@gmail.com>
2026-05-18 21:49:53 -0700

    Add files via upload

---
+++ Pekan 13/tejja arum_035/koneksi.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_kampus";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi Gagal : " . mysqli_connect_error());
+}
+
+?>
