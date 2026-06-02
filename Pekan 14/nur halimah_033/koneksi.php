ae2f60138c985817c2335d4ee1bbe448f41818c5
nurmahalim24 <nurmahalim24@gmail.com>
2026-06-02 08:48:03 +0700

    Create koneksi.php

---
+++ Pekan 14/nur halimah_033/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$koneksi = mysqli_connect("localhost", "root", "", "db_kampus");
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
